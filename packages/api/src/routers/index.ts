c1b9fbdcb039c6ad95fc9612e87c7e520ccd0f35
sultandevin <sultan.suyudi@gmail.com>
2025-11-13 16:55:16 +0700

    build: add logging in workers

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -5,24 +5,29 @@ import * as z from "zod";
 import { protectedProcedure, publicProcedure } from "../index";
 
 export const appRouter = {
-	healthCheck: publicProcedure.handler(() => {
-		return "OK";
-	}),
-	privateData: protectedProcedure.handler(({ context }) => {
-		return {
-			message: "This is private",
-			user: context.session?.user,
-		};
-	}),
-	greet: publicProcedure
-		.input(z.string())
-		.output(z.string())
-		.handler(({ input }) => {
-			return `hello ${input}`;
-		}),
-	users: publicProcedure.handler(async ({ context }) => {
-		return await db(context.env).select().from(user);
-	}),
+  healthCheck: publicProcedure.handler(() => {
+    return "OK";
+  }),
+  privateData: protectedProcedure.handler(({ context }) => {
+    return {
+      message: "This is private",
+      user: context.session?.user,
+    };
+  }),
+  greet: publicProcedure
+    .input(z.string())
+    .output(z.string())
+    .handler(({ input }) => {
+      return `hello ${input}`;
+    }),
+  users: publicProcedure
+    .route({
+      method: "GET",
+      path: "/users",
+    })
+    .handler(async ({ context }) => {
+      return await db(context.env).select().from(user);
+    }),
 };
 export type AppRouter = typeof appRouter;
 export type AppRouterClient = RouterClient<typeof appRouter>;
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,10 +2,10 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 export function db(env: CloudflareBindings) {
-	const sql = neon(env.DATABASE_URL);
+  const sql = neon(env.DATABASE_URL);
 
-	return drizzle({
-		client: sql,
-		casing: "snake_case",
-	});
+  return drizzle({
+    client: sql,
+    casing: "snake_case",
+  });
 }
