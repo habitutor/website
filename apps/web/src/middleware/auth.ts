9eb73af2ad75cfbed1889a4428e39468d9fbe5bd
sultandevin <sultan.suyudi@gmail.com>
2025-11-15 12:59:16 +0700

    chore: update logging

--- apps/web/src/middleware/auth.ts
+++ apps/web/src/middleware/auth.ts
@@ -3,13 +3,13 @@ import { authClient } from "@/lib/auth-client";
 
 export const authMiddleware = createMiddleware().server(
   async ({ next, request }) => {
+    console.log("VITE_SERVER_URL", import.meta.env.VITE_SERVER_URL);
     const session = await authClient.getSession({
       fetchOptions: {
         headers: request.headers,
         throw: true,
       },
     });
-    console.log("VITE_SERVER_URL", import.meta.env.VITE_SERVER_URL);
     return next({
       context: { session },
     });
