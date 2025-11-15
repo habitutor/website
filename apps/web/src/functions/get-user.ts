f237a164d25a763f64a6f8fd7f85d9ea0a4cadcd
sultandevin <sultan.suyudi@gmail.com>
2025-11-15 15:22:38 +0700

    fix: remove server-side auth calls

--- apps/web/src/functions/get-user.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { authMiddleware } from "@/middleware/auth";
-
-export const getUser = createServerFn()
-	.middleware([authMiddleware])
-	.handler(async ({ context }) => {
-		return context.session;
-	});
