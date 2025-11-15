d501fd10eab5f34ca7ee3fc56d9a880da84c5cd9
sultandevin <sultan.suyudi@gmail.com>
2025-11-15 15:03:30 +0700

    fix(middleware): use auth() instead of authClient to calls in the server side

--- apps/web/src/middleware/auth.ts
+++ apps/web/src/middleware/auth.ts
@@ -1,14 +1,11 @@
+import { env } from "cloudflare:workers";
+import { auth } from "@habitutor/auth";
 import { createMiddleware } from "@tanstack/react-start";
-import { authClient } from "@/lib/auth-client";
 
 export const authMiddleware = createMiddleware().server(
   async ({ next, request }) => {
-    console.log("VITE_SERVER_URL", import.meta.env.VITE_SERVER_URL);
-    const session = await authClient.getSession({
-      fetchOptions: {
-        headers: request.headers,
-        throw: true,
-      },
+    const session = await auth(env).api.getSession({
+      headers: request.headers,
     });
     return next({
       context: { session },
