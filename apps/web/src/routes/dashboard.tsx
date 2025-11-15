e00ece3afeb7f573388ac8c0191188dac20d0de5
sultandevin <sultan.suyudi@gmail.com>
2025-11-15 10:58:40 +0700

    chore: test authclient in beforeLoad

--- apps/web/src/routes/dashboard.tsx
+++ apps/web/src/routes/dashboard.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { getUser } from "@/functions/get-user";
+import { authClient } from "@/lib/auth-client";
 import { orpc } from "@/utils/orpc";
 
 export const Route = createFileRoute("/dashboard")({
   component: RouteComponent,
   beforeLoad: async () => {
-    const session = await getUser();
+    const session = await authClient.getSession();
     return { session };
   },
   loader: async ({ context }) => {
@@ -26,7 +26,7 @@ function RouteComponent() {
   return (
     <div className="mx-auto flex w-full max-w-4xl flex-col gap-2 pt-20">
       <h1 className="font-bold text-lg">Dashboard</h1>
-      <p>Welcome {session?.user.name}</p>
+      <p>Welcome {session.data?.user.name}</p>
       <div className="flex flex-col gap-2 rounded-md border bg-background p-4">
         <h2 className="font-bold">API:</h2>
         {privateData.data?.message}
