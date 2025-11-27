891f9e875c7cfd548ca374eb13d1a6c609ba3bbd
sultandevin <sultan.suyudi@gmail.com>
2025-11-27 16:56:59 +0700

    build: create production dockerfiles

--- packages/db/src/schema/flashcard.ts
+++ packages/db/src/schema/flashcard.ts
@@ -6,6 +6,7 @@ import {
   pgTable,
   primaryKey,
   text,
+  timestamp,
 } from "drizzle-orm/pg-core";
 import { user } from "./auth";
 import { question, questionAnswerOption } from "./practice-pack";
