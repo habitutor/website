72ce46af06b4e4511d067d65772cef1b5f4340a8
sultandevin <sultan.suyudi@gmail.com>
2025-11-27 11:50:08 +0700

    feat: create getting todays flashcard feature

--- packages/api/src/routers/flashcard.ts
+++ packages/api/src/routers/flashcard.ts
@@ -1,19 +1,90 @@
+import { db } from "@habitutor/db";
+import { userFlashcard } from "@habitutor/db/schema/flashcard";
+import { ORPCError } from "@orpc/client";
+import { and, eq, gte } from "drizzle-orm";
 import { authed } from "..";
-import { db } from "../../../db/src";
-import { userFlashcard } from "../../../db/src/schema/flashcard";
+
+// Cutoff in 30 Days
+const FLASHCARD_CUTOFF_LIMIT = 30;
 
 const today = authed
-	.route({
-		path: "/flashcard/today",
-		method: "GET",
-		tags: ["Flashcard"],
-	})
-	.handler(async ({ context }) => {
-		const [flashcard] = await db.select().from(userFlashcard).limit(1);
+  .route({
+    path: "/flashcard/today",
+    method: "GET",
+    tags: ["Flashcard"],
+  })
+  .handler(async ({ context }) => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-		return flashcard;
-	});
+    let flashcard:
+      | (Awaited<ReturnType<typeof db.query.userFlashcard.findFirst>> & {
+        question: Awaited<ReturnType<typeof db.query.question.findFirst>>;
+      })
+      | undefined = await db.query.userFlashcard.findFirst({
+        where: and(
+          eq(userFlashcard.userId, context.session.user.id),
+          eq(userFlashcard.assignedDate, today),
+        ),
+        with: {
+          question: {
+            with: {
+              answerOptions: true,
+            },
+          },
+        },
+      });
+
+    if (!flashcard) {
+      const dateBoundary = new Date(
+        Date.now() - FLASHCARD_CUTOFF_LIMIT * 24 * 3600 * 1000,
+      );
+
+      const availableQuestion = await db.query.question.findFirst({
+        where: and(
+          eq(userFlashcard.userId, context.session.user.id),
+          gte(userFlashcard.assignedDate, dateBoundary),
+        ),
+        with: {
+          answerOptions: true,
+        },
+      });
+
+      if (!availableQuestion)
+        throw new ORPCError("NOT_FOUND", {
+          message: "Gagal menemukan flashcard hari ini.",
+          cause: "Gagal menemukan pertanyaan yang tersedia.",
+        });
+
+      const [createdFlashcard] = await db
+        .insert(userFlashcard)
+        .values({
+          userId: context.session.user.id,
+          assignedDate: today,
+          questionId: availableQuestion.id,
+        })
+        .returning();
+
+      if (!createdFlashcard)
+        throw new ORPCError("NOT_FOUND", {
+          message: "Gagal menemukan flashcard hari ini.",
+          cause: "Gagal membuat flashcard baru.",
+        });
+
+      flashcard = {
+        ...createdFlashcard,
+        question: availableQuestion,
+      };
+    }
+
+    if (!flashcard)
+      throw new ORPCError("NOT_FOUND", {
+        message: "Gagal menemukan flashcard hari ini.",
+      });
+
+    return flashcard;
+  });
 
 export const flashcardRouter = {
-	today,
+  today,
 };
