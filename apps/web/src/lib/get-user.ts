3fbd31e48aff2aa28dd4ec6c7e77dbc4648fb802
sultandevin <sultan.suyudi@gmail.com>
2025-12-02 23:58:07 +0700

    feat: update index.css theme to match figma

--- apps/web/src/lib/get-user.ts
+++ apps/web/src/lib/get-user.ts
@@ -1,25 +1,34 @@
-import { createIsomorphicFn } from "@tanstack/react-start";
+import { createIsomorphicFn, createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
 import { authClient } from "./auth-client";
 
-export const getUser = createIsomorphicFn()
-	.server(async () => {
-		const { data } = await authClient.getSession({
-			fetchOptions: {
-				headers: getRequestHeaders(),
-			},
-		});
+// export const getUser = createIsomorphicFn()
+//   .server(async () => {
+//     const { data } = await authClient.getSession({
+//       fetchOptions: {
+//         headers: getRequestHeaders(),
+//       },
+//     });
+//
+//     return data;
+//   })
+//   .client(async () => {
+//     return (await authClient.getSession()).data;
+//   });
+export const getUser = createServerFn().handler(async () => {
+  const { data } = await authClient.getSession({
+    fetchOptions: {
+      headers: getRequestHeaders(),
+    },
+  });
 
-		return data;
-	})
-	.client(async () => {
-		return (await authClient.getSession()).data;
-	});
+  return data;
+});
 
-export async function isAuthenticated(): Promise<boolean> {
-	const user = await authClient.getSession();
+export async function isAuthenticated() {
+  const user = await authClient.getSession();
 
-	if (user) return true;
+  if (user) return true;
 
-	return false;
+  return false;
 }
