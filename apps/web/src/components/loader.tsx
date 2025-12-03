213e49aa98d988ab70668918688dea364b8061ce
sultandevin <sultan.suyudi@gmail.com>
2025-12-03 15:11:43 +0700

    build: add turborepo remote caching config

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -3,7 +3,7 @@ import { AndroidLogo } from "@phosphor-icons/react";
 export default function Loader() {
   return (
     <div className="flex h-full items-center justify-center pt-8">
-      <AndroidLogo className="h-8 w-8 animate-spin text-gray-600" />
+      <AndroidLogo className="h-9 w-8 animate-spin text-gray-600" />
     </div>
   );
 }
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,34 +4,40 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "pg",
-		schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || "http://localhost:3000"],
-	emailAndPassword: {
-		enabled: true,
-	},
-	session: {
-		cookieCache: {
-			enabled: true,
-			maxAge: 60,
-		},
-	},
-	secret: process.env.BETTER_AUTH_SECRET,
-	baseURL: process.env.BETTER_AUTH_URL,
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-			secure: process.env.NODE_ENV === "production",
-			httpOnly: true,
-		},
-		...(process.env.NODE_ENV === "production" && {
-			crossSubDomainCookies: {
-				enabled: true,
-				domain: new URL(process.env.CORS_ORIGIN || "http://localhost:3000")
-					.hostname,
-			},
-		}),
-	},
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || "http://localhost:3000"],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60,
+    },
+  },
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+    },
+    ...(process.env.NODE_ENV === "production" && {
+      crossSubDomainCookies: {
+        enabled: true,
+        domain: new URL(process.env.CORS_ORIGIN || "http://localhost:3000")
+          .hostname,
+      },
+    }),
+  },
 });
