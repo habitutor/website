c3c98b26e4731baabb643d6ab50dbecd0aba9bae
sultandevin <sultan.suyudi@gmail.com>
2025-12-08 20:09:33 +0700

    feat(dashboard): add time remembrance for dashboard alert

--- apps/web/src/routes/_authenticated/-components/dismissable-alert.tsx
+++ apps/web/src/routes/_authenticated/-components/dismissable-alert.tsx
@@ -1,9 +1,22 @@
 import { ArrowRightIcon, XIcon } from "@phosphor-icons/react";
-import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const TIME_ELAPSED_BEFORE_SHOWING_ALERT_AGAIN = 1000 * 60 * 60 * 24; // 24 hours
 
 export const DismissableAlert = () => {
-  const [closed, setClosed] = useState(false);
+  const [closed, setClosed] = useState<boolean>(() => {
+    const previousCloseState = localStorage.getItem("dismissable-alert-closed");
+    if (!previousCloseState) return false;
+
+    const lastClosed = new Date(previousCloseState);
+    const now = new Date();
+
+    return (
+      now.getTime() - lastClosed.getTime() <
+      TIME_ELAPSED_BEFORE_SHOWING_ALERT_AGAIN
+    );
+  });
 
   if (closed) return null;
 
@@ -15,7 +28,17 @@ export const DismissableAlert = () => {
       </div>
 
       <div className="flex flex-col items-end justify-between gap-4">
-        <Button onClick={() => setClosed(true)} variant={"ghost"} size={"icon"}>
+        <Button
+          onClick={() => {
+            setClosed(true);
+            localStorage.setItem(
+              "dismissable-alert-closed",
+              new Date().toISOString(),
+            );
+          }}
+          variant={"ghost"}
+          size={"icon"}
+        >
           <XIcon strokeWidth={24} />
         </Button>
         <Button size={"icon"} asChild>
