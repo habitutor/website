5e61ba096cd0c197459a92c06360d3cf58a072ba
sultandevin <sultan.suyudi@gmail.com>
2025-12-21 13:05:23 +0700

    fix(flashcard): migrate getting current streak to via session

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,16 +1,23 @@
+import { db } from "@habitutor/db";
+import { user } from "@habitutor/db/schema/auth";
 import { o } from "./lib/orpc";
 import { requireAdmin } from "./middlewares/rbac";
 
 export const pub = o;
 const requireAuth = o.middleware(async ({ context, next, errors }) => {
-	if (!context.session?.user) {
-		throw errors.UNAUTHORIZED();
-	}
-	return next({
-		context: {
-			session: context.session,
-		},
-	});
+  if (!context.session?.user) throw errors.UNAUTHORIZED();
+  if (
+    context.session.user.flashcardStreak === 0 &&
+    Date.now() - context.session.user.lastCompletedFlashcardAt.getTime() >= 2 * 24 * 3600 * 1000
+  ) {
+    await db.update(user).set({ flashcardStreak: 0 });
+  }
+
+  return next({
+    context: {
+      session: context.session,
+    },
+  });
 });
 
 export const authed = pub.use(requireAuth);
