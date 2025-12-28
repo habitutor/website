6081de4116c3c67b70188b50802bebb4d1f3183a
sultandevin <sultan.suyudi@gmail.com>
2025-12-28 12:50:05 +0700

    chore: add new error code

--- packages/api/src/lib/orpc.ts
+++ packages/api/src/lib/orpc.ts
@@ -2,7 +2,8 @@ import { os } from "@orpc/server";
 import type { Context } from "../context";
 
 export const o = os.$context<Context>().errors({
-	NOT_FOUND: { message: "Gagal menemukan resource yang diminta." },
-	UNAUTHORIZED: { message: "Anda tidak memiliki akses ke resource ini." },
-	UNPROCESSABLE_CONTENT: { message: "Permintaan anda tidak dapat diproses." },
+  NOT_FOUND: { message: "Gagal menemukan resource yang diminta." },
+  UNAUTHORIZED: { message: "Anda tidak memiliki akses ke resource ini." },
+  UNPROCESSABLE_CONTENT: { message: "Permintaan anda tidak dapat diproses." },
+  INTERNAL_SERVER_ERROR: { message: "Terjadi kesalahan pada sisi kami, silahkan coba lagi." },
 });
