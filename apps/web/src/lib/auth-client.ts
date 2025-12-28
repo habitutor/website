7e79d2db588869b2731d6136405b781f741ac178
sultandevin <sultan.suyudi@gmail.com>
2025-12-28 12:12:16 +0700

    fix: add server-side check for auth server url

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: process.env.SERVER_URL || import.meta.env.VITE_SERVER_URL,
+	baseURL: typeof window === "undefined" ? process.env.SERVER_URL : import.meta.env.VITE_SERVER_URL,
 	plugins: [inferAdditionalFields<typeof auth>()],
 });
 
