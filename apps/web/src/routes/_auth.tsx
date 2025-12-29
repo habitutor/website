e35f1a8a290a7e8d457b56e3f1e93eb7d3bdefde
sultandevin <sultan.suyudi@gmail.com>
2025-12-29 13:44:09 +0700

    fix: add api domain to CORS and trustedOrigins

--- apps/web/src/routes/_auth.tsx
+++ apps/web/src/routes/_auth.tsx
@@ -2,22 +2,22 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { getUser } from "@/lib/get-user";
 
 export const Route = createFileRoute("/_auth")({
-  beforeLoad: async () => {
-    const session = await getUser();
+	beforeLoad: async () => {
+		const session = await getUser();
 
-    return { session };
-  },
-  loader: ({ context }) => {
-    if (context.session)
-      throw redirect({
-        to: "/dashboard",
-      });
+		return { session };
+	},
+	loader: ({ context }) => {
+		if (context.session)
+			throw redirect({
+				to: "/dashboard",
+			});
 
-    console.log("Before Load: ", context.session);
-  },
-  component: AuthLayout,
+		console.log("Before Load: ", context.session);
+	},
+	component: AuthLayout,
 });
 
 function AuthLayout() {
-  return <Outlet />;
+	return <Outlet />;
 }
