7f915d033de5b756bad445bacc7b59f5ff5f1961
sultandevin <sultan.suyudi@gmail.com>
2025-12-29 11:54:46 +0700

    chore: biome lint

--- apps/web/src/hooks/use-throttled-callback.ts
+++ apps/web/src/hooks/use-throttled-callback.ts
@@ -34,7 +34,7 @@ export function useThrottledCallback<T extends (...args: any[]) => any>(
 } {
 	const handler = useMemo(
 		() => throttle<T>(fn, wait, options),
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		// biome-ignore lint/correctness/useExhaustiveDependencies: gatau anjir
 		dependencies,
 	);
 
